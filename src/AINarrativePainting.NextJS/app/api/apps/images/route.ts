5cfa4160d0c3bc44e75ea40c0671cc2ff8b66d4e
monster <rwecholili@gmail.com>
2024-12-19 11:24:34 +0800

    chore: set up production environment variables in GitHub workflow and update build script

--- src/AINarrativePainting.NextJS/app/api/apps/images/route.ts
+++ src/AINarrativePainting.NextJS/app/api/apps/images/route.ts
@@ -1,12 +1,5 @@
 import supabase from "@/app/api/utils/supabase"
 import { NextRequest, NextResponse } from "next/server"
-import OSS from "ali-oss"
-const client = new OSS({
-  region: "oss-cn-shanghai",
-  accessKeyId: process.env.ALIYUN_ACCESS_KEY_ID!,
-  accessKeySecret: process.env.ALIYUN_ACCESS_KEY_SECRET!,
-  bucket: "aishuohua",
-})
 
 export async function GET(
   request: NextRequest
@@ -25,13 +18,14 @@ export async function GET(
       .map((item) => {
         return {
           prompt: item.prompt,
-          filenames: item.filenames
-            .split(",")
-            .map((filename: string) =>
-              client.signatureUrl(
-                `dify-oss/tools/d8f62afb-7c20-4a9c-b0de-fbe02bb2a02e/${filename}`
-              )
-            ),
+          filenames: [],
+          // filenames: item.filenames
+          //   .split(",")
+          //   .map((filename: string) =>
+          //     client.signatureUrl(
+          //       `dify-oss/tools/d8f62afb-7c20-4a9c-b0de-fbe02bb2a02e/${filename}`
+          //     )
+          //   ),
         }
       }),
   })
